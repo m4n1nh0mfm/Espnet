ec55b227b86f8a269d8f8ac7435ea33b54e44206
Elisrenan <elisrenan@gmail.com>
2016-12-19 23:20:48 -0300

    Classes

---
+++ EsporteNet/EsporteNet/Model/Contato/ContatoUsu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EsporteNet.Model.Contato
+{
+    public class ContatoUsu
+    {
+        private int cod_usu { get; set; }
+        private string email { get; set; }
+        private string telefone { get; set; }
+        private string celular { get; set; }
+
+        public ContatoUsu()
+        {
+
+        }
+        public ContatoUsu(int cod_usu)
+        {
+            this.cod_usu = cod_usu;
+        }
+    }
+}
